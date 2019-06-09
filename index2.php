b0d34c8d4b0978db08af7698b01e166f47dc80fb
sathyasekar <32748333+sathyasekar@users.noreply.github.com>
2019-06-09 22:49:19 +0200

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,51 @@
+<?php
+	// Header
+require 'partial/header.php';
+?>
+
+  <body>
+
+ <?php
+	// Nav
+require 'partial/nav.php';
+?>
+
+	<!--  Welcome Section  -->
+	<div id="ww">
+	    <div class="container">
+			<div class="row">
+				<div class="col-lg-8 col-lg-offset-2 centered">
+					<img class="profile" src="assets/img/sathya.png" height="40%" width="40%" style="margin:50px 0px" alt="sathya">
+					<h1>Hi, I am Sathya SEKAR !</h1>
+					<p>Hello everybody. I'm Sathya, a second year student in web school. I am interested in design and more specificly in User Experience Design</p>
+				
+				</div>
+			</div>
+	    </div> 
+	</div>
+	
+	
+	<!--  Projects Section -->
+	
+	<div class="container pt">
+		<div class="row mt centered">	
+			<div class="col-lg-4">
+				<a class="zoom green" href="work01.php"><img class="img-responsive" src="assets/img/portfolio/mandala.png" alt="" /></a>
+				<p>MANDALA</p>
+			</div>
+			<div class="col-lg-4">
+				<a class="zoom green" href="work02.php"><img class="img-responsive" src="assets/img/portfolio/matte.png" alt="" /></a>
+				<p>MATTE PAINTING</p>
+			</div>
+			<div class="col-lg-4">
+				<a class="zoom green" href="work03.php"><img class="img-responsive" src="assets/img/portfolio/port.png" alt="" /></a>
+				<p>REMAKE</p>
+			</div>
+		</div>
+	</div>
+	
+	
+	<?php
+	// Footer
+require 'partial/footer.php';
+?>
